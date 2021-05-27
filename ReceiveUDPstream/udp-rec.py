7746135177649f060894710df13bdb42f46abe7b
IDCrypticore <82649510+IDCrypticore@users.noreply.github.com>
2021-05-27 09:58:40 +0200

    Update udp-rec.py

--- ReceiveUDPstream/udp-rec.py
+++ ReceiveUDPstream/udp-rec.py
@@ -12,26 +12,27 @@ def signal_handler(sig, frame):
     p.set_state(Gst.State.NULL)
     sys.exit(0)
 
-# Initialize gstreamer
+# Initialize GStreamer
 GObject.threads_init()
 Gst.init(None)
 
-# Define pipeline 
-#gst_str = "nvarguscamerasrc ! video/x-raw(memory:NVMM),format=(string)NV12,width=(int)640,height=(int)480, framerate=30/1 ! nvvidconv ! xvimagesink "
+# Defining GStreamer pipeline, searching for stream on port 5000.
+# The matroskamux takes the streaming and mux it to a matroska file (.mkv), and the recorded video is thereby saved in the home directory as test.mkv.
 gst_str = "udpsrc port=5000 ! application/x-rtp,encoding-name=H264 ! rtpjitterbuffer latency=1000 drop-on-latency=false ! rtph264depay ! h264parse! avdec_h264 ! matroskamux ! filesink location=test.mkv"
 
 # Create the pipeline
 p = Gst.parse_launch (gst_str)
 
-# Register signal handler for proper termination if receiving SIGINT such as Ctrl-C
+# Register signal handler in order to stop the recording if receiving SIGINT, for instance Ctrl-C
 signal.signal(signal.SIGINT, signal_handler)
 
-# Start the pipeline
+# Starting the pipeline.
 p.set_state(Gst.State.READY)
 p.set_state(Gst.State.PAUSED)
 p.set_state(Gst.State.PLAYING)
 
-# Run for 1000s 
+# Running for 1000 seconds, before exit.
+# This can be set to another desired value.
 time.sleep(1000)
 
 # Done. Stop the pipeline before clean up on exit.
