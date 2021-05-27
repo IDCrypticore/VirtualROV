2eba95198905621af5346b385a141cde411ae834
IDCrypticore <82649510+IDCrypticore@users.noreply.github.com>
2021-05-27 09:54:07 +0200

    Update udp-receive.py

--- ReceiveUDPstream/udp-receive.py
+++ ReceiveUDPstream/udp-receive.py
@@ -12,18 +12,18 @@ def signal_handler(sig, frame):
     p.set_state(Gst.State.NULL)
     sys.exit(0)
 
-# Initialize gstreamer
+# Initialize GStreamer.
 GObject.threads_init()
 Gst.init(None)
 
-# Define pipeline 
+# Defining the GStreamer pipeline, searching for stream on port 5000.
 #gst_str = "nvarguscamerasrc ! video/x-raw(memory:NVMM),format=(string)NV12,width=(int)640,height=(int)480, framerate=30/1 ! nvvidconv ! xvimagesink "
 gst_str = "udpsrc port=5000 ! application/x-rtp,encoding-name=H264 ! rtpjitterbuffer latency=1000 drop-on-latency=false ! rtph264depay ! h264parse! avdec_h264 ! xvimagesink"
 
-# Create the pipeline
+# Creating the pipeline.
 p = Gst.parse_launch (gst_str)
 
-# Register signal handler for proper termination if receiving SIGINT such as Ctrl-C
+# Register signal handler in order to terminate the session if receiving SIGINT, for instance Ctrl-C.
 signal.signal(signal.SIGINT, signal_handler)
 
 # Start the pipeline
@@ -31,7 +31,8 @@ p.set_state(Gst.State.READY)
 p.set_state(Gst.State.PAUSED)
 p.set_state(Gst.State.PLAYING)
 
-# Run for 1000s 
+# Running session for 1000 seconds, before exit.
+# This can be set to another desired value.
 time.sleep(1000)
 
 # Done. Stop the pipeline before clean up on exit.
